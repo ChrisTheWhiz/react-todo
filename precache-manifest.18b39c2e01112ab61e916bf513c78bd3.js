996e060ab021fe314303e8064cbbc78b8f84ae48
ChrisTheWhiz <piersinaru.cristian@gmail.com>
2020-01-11 18:49:03 +0200

    Updates

---
+++ precache-manifest.18b39c2e01112ab61e916bf513c78bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3038c603f907ea1ca72e866caf894f3b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "a37a969c441c7ad57c17",
+    "url": "/react-todo/static/css/main.5f74b17d.chunk.css"
+  },
+  {
+    "revision": "57d5051dce4f3881adc0",
+    "url": "/react-todo/static/js/2.6a23ee09.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-todo/static/js/2.6a23ee09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a37a969c441c7ad57c17",
+    "url": "/react-todo/static/js/main.81d19bf5.chunk.js"
+  },
+  {
+    "revision": "e0ca021b3f8e35e431e7",
+    "url": "/react-todo/static/js/runtime-main.d5b46dbe.js"
+  }
+]);
