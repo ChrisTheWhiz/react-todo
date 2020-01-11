5e584a252b0337e06e389ae5cce2714077614eb3
ChrisTheWhiz <piersinaru.cristian@gmail.com>
2020-01-11 03:20:21 +0200

    implemented drag and drop for projects + option to whole projects by dragging to trash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import './App.scss';
 import './Styles.scss';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+
 
 function App() {
 
@@ -43,23 +45,23 @@ function App() {
                 },
             ]
         },
-        {
-            projectName: 'Groceries',
-            todos: [
-                {
-                    content: '1kg Carrots',
-                    isCompleted: false,
-                },
-                {
-                    content: '2 Cauliflowers',
-                    isCompleted: false,
-                },
-                {
-                    content: '1kg Button Mushrooms',
-                    isCompleted: false,
-                },
-            ]
-        }
+            // {
+            //     projectName: 'Groceries',
+            //     todos: [
+            //         {
+            //             content: '1kg Carrots',
+            //             isCompleted: false,
+            //         },
+            //         {
+            //             content: '2 Cauliflowers',
+            //             isCompleted: false,
+            //         },
+            //         {
+            //             content: '1kg Button Mushrooms',
+            //             isCompleted: false,
+            //         },
+            //     ]
+            // }
     ]);
 
     function handleInputChange(e, projectIndex, todoIndex) {
@@ -82,13 +84,13 @@ function App() {
                 tempState[projectIndex].todos.splice(todoIndex + 1, 0, {content: '', isCompleted: false});
                 setTimeout(() => {
                     // document.forms[0].elements[i + 1].focus();
-                    document.forms[0].elements[projectIndex+todoIndex+3].focus();
+                    document.forms[0].elements[projectIndex + todoIndex + 3].focus();
                 }, 0);
             } else {
                 // change in a project name
                 tempState[projectIndex].todos.splice(0, 0, {content: '', isCompleted: false});
                 setTimeout(() => {
-                    document.forms[0].elements[projectIndex+1].focus();
+                    document.forms[0].elements[projectIndex + 1].focus();
                 }, 0);
             }
             setTodos(tempState);
@@ -113,49 +115,111 @@ function App() {
         setTodos(tempState);
     }
 
+    function onDragEnd(result) {
+        const {destination, source} = result;
+        const tempState = [...todos];
+        setTrashVisibility(false);
+
+        if (!destination) {
+            return;
+        } else if (destination.droppableId === 'trash') {
+            tempState.splice(parseInt(source.index.toString()), 1);
+            setTodos(tempState);
+            return;
+        }
+
+        if (destination.index === source.index) return;
+
+        tempState.splice(parseInt(source.index.toString()), 1);
+        tempState.splice(destination.index, 0, todos[source.index]);
+
+        setTodos(tempState);
+    }
+
+    function setTrashVisibility(mode) {
+        const el = document.getElementById('trash');
+        mode ? el.style.opacity = '1' : el.style.opacity = '0';
+    }
+
     return (
         <div className="app">
             <div className="header">
                 <h2>React Todo App</h2>
                 <button>Clear Local Storage</button>
             </div>
-            <form className="todo-list">
-                {todos.map((project, projectIndex) => (
-                    <div className="todo-category" key={projectIndex}>
-                        <input className="category-header"
-                               type="text"
-                               value={project.projectName}
-                               onChange={e => handleInputChange(e, projectIndex)}
-                               onKeyDown={e => handleInputKeyDown(e, projectIndex)}
-                        />
-                        <ul>
-                            {project.todos.map((todo, todoIndex) => (
-                                <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`} key={todoIndex}
-                                     onMouseEnter={() => setCloseButton(true, projectIndex, todoIndex)}
-                                     onMouseLeave={() => setCloseButton(false, projectIndex, todoIndex)}
+            <DragDropContext onDragEnd={onDragEnd} onDragStart={() => setTrashVisibility(true)}>
+                <Droppable droppableId="trash">
+                    {(provided, snapshot) => (
+                        <div className={`trash ${snapshot.isDraggingOver ? 'active' : ''}`}
+                             ref={provided.innerRef}
+                             {...provided.droppableProps}
+                             id="trash"
+                        >TRASHCAN
+                            <div style={{display: 'none'}}>{provided.placeholder}</div>
+                        </div>
+                    )}
+                </Droppable>
+                <Droppable droppableId="todo-list">
+                    {(provided) => (
+                        <form className="todo-list"
+                              ref={provided.innerRef}
+                              {...provided.droppableProps}>
+                            {provided.placeholder}
+                            {todos.map((project, projectIndex) => (
+                                <Draggable
+                                    key={projectIndex}
+                                    draggableId={projectIndex.toString()}
+                                    index={projectIndex}
                                 >
-                                    {/* eslint-disable-next-line*/}
-                                    <div className="checkbox"
-                                         onClick={() => toggleTodoCompleteAtIndex(projectIndex, todoIndex)}>
-                                        {todo.isCompleted && (
-                                            <span>&#x2714;</span>
-                                        )}
-                                    </div>
-                                    <input
-                                        type="text"
-                                        value={todo.content}
-                                        onChange={e => handleInputChange(e, projectIndex, todoIndex)}
-                                        onKeyDown={e => handleInputKeyDown(e, projectIndex, todoIndex)}
-                                    />
-                                    {todo.showCloseButton? <button type="button" className='close'
-                                                                   onClick={() => handleRemoveCommand(projectIndex, todoIndex)}
-                                    >x</button>: null}
-                                </div>
+                                    {(provided) => (
+                                        <div className="todo-category"
+                                             key={projectIndex}
+                                             {...provided.draggableProps}
+                                             ref={provided.innerRef}
+                                        >
+                                            <span className="drag-handle"
+                                                  {...provided.dragHandleProps}
+                                            >≡</span>
+                                            <input
+                                                className="category-header"
+                                                type="text"
+                                                value={project.projectName}
+                                                onChange={e => handleInputChange(e, projectIndex)}
+                                                onKeyDown={e => handleInputKeyDown(e, projectIndex)}
+                                            />
+                                            <ul>
+                                                {project.todos.map((todo, todoIndex) => (
+                                                    <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}
+                                                         key={todoIndex}
+                                                         onMouseEnter={() => setCloseButton(true, projectIndex, todoIndex)}
+                                                         onMouseLeave={() => setCloseButton(false, projectIndex, todoIndex)}
+                                                    >
+                                                        {/* eslint-disable-next-line*/}
+                                                        <div className="checkbox"
+                                                             onClick={() => toggleTodoCompleteAtIndex(projectIndex, todoIndex)}>
+                                                            {todo.isCompleted && (
+                                                                <span>&#x2714;</span>
+                                                            )}
+                                                        </div>
+                                                        <input
+                                                            type="text"
+                                                            value={todo.content}
+                                                            onChange={e => handleInputChange(e, projectIndex, todoIndex)}
+                                                            onKeyDown={e => handleInputKeyDown(e, projectIndex, todoIndex)}
+                                                        />
+                                                        {todo.showCloseButton ? <button type="button" className='close'
+                                                                                        onClick={() => handleRemoveCommand(projectIndex, todoIndex)}
+                                                        >x</button> : null}
+                                                    </div>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )}
+                                </Draggable>
                             ))}
-                        </ul>
-                    </div>
-                ))}
-            </form>
+                        </form>)}
+                </Droppable>
+            </DragDropContext>
         </div>
     );
 }
